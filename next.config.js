b5832ac86d0db305b129b313073b725d8ca35ea0
Oliver N <72499511+NemesOliver@users.noreply.github.com>
2021-12-07 14:05:44 +0000

    convert to PWA

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+  },
+});
+
 module.exports = {
   reactStrictMode: true,
-}
+};
